0fd7f12147bf881884cdfa745d0b6b8f3466d3eb
Aashish Subramanian <aashish.subramanian26@gmail.com>
2025-04-07 00:54:34 -0500

    Changing the text

--- src/content/home.ts
+++ src/content/home.ts
@@ -1,6 +1,6 @@
 export const homeContent = {
   role: "Game and Software Developer | Computer Engineering @ UIUC",
-  description: "Hi, I'm Aashish! I’m a software engineer from Illinois with a passion for building interactive experiences in video games. Currently, I’m a senior at the University of Illinois at Urbana-Champaign, majoring in Computer Engineering with a minor in Game Studies and Design. I love bringing ideas to life through code—whether it’s crafting engaging gameplay systems or solving technical challenges. I’ve worked on solo assembly games to team-based projects using modern game engines and frameworks. I'm always excited to learn, collaborate, and push creative boundaries through technology. Open to full-time opportunities in 2025!",
+  description: "Hi, I’m Aashish! I’m a senior in Computer Engineering at UIUC with a minor in Game Studies and Design. I’m passionate about both software engineering and game development—whether I’m building engaging systems or solving tough technical problems, I’m driven to innovate and create meaningful experiences. Open to full-time opportunities in 2025!",
   gameDevProject: "The Shifter",
   gameDevProjectTags: ["Unreal Engine", "Game Jam", "3-Person Team"],
   gameDevImgSrc: "/home/game-dev-shifter.png",
