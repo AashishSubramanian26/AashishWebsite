87580238f77276f96d9c4ff447aa9c232949a6d3
Aashish Subramanian <aashish.subramanian26@gmail.com>
2026-01-12 16:13:32 -0600

    updated resumes

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -22,7 +22,7 @@ const Resume = () => {
       </Box>
       <Box justifySelf="center" paddingTop={10} width="80%">
         <object
-          data="Aashish Subramanian Resume - Software Engineering.pdf"
+          data="Aashish Subramanian's Resume.pdf"
           type="application/pdf" 
           width="100%"
           height="1200px"
